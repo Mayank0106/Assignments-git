6ce2bc34710191de660f32e293e0ee11a5838b48
Mayank Sharma <ms8295111175@gmail.com>
2018-08-19 09:52:47 +0530

    My first commit

---
+++ pattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class pattern {
+  public static void main(String[] args) {
+    int a,n;
+    for(int i=1;i<=4;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
